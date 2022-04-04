0a40d552ea578355487b8c6452ee5ac4690fb51c
ismail <ismail95190@gmail.com>
2022-04-05 01:24:16 +0200

    search function

--- wp-content/themes/marmishlag12/front-page.php
+++ wp-content/themes/marmishlag12/front-page.php
@@ -22,15 +22,7 @@
                                 <option value="development">Restaurant</option>
                                 <option value="marketing">Marketing</option>
                             </select>
-                            <input placeholder="Your favorite food" class="w-full p-4 rounded-full" type="text">
-                            <button type="button" title="Start buying" class="ml-auto py-3 px-6 rounded-full text-center transition bg-gradient-to-b from-yellow-200 to-yellow-300 hover:to-red-300 active:from-yellow-400 focus:from-red-400 md:px-12">
-                                <span class="hidden text-yellow-900 font-semibold md:block">
-Search
-                                </span>
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 mx-auto text-yellow-900 md:hidden" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                </svg>
-                            </button>
+                            <?php get_search_form(); ?>
                         </div>
                     </form>
                     <p class="mt-8 text-gray-700 lg:w-10/12">Sit amet consectetur adipisicing elit. <a href="#" class="text-yellow-700">connection</a> tenetur nihil quaerat suscipit, sunt dignissimos.</p>
